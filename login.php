66e94b8fd9a183242654ff93aff4e500ba11ec1b
otake <otake.hiroyo@gmail.com>
2017-04-01 20:32:22 +0900

    ログイン画面にバリデーション追加

--- login.php
+++ login.php
@@ -8,6 +8,32 @@ if (!empty($_SESSION['id']))
 		exit;
 }
 
+require_once('config.php');
+require_once('functions.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$errors = array();
+
+	// バリデーション
+	if ($name == '')
+	{
+		$errors['name'] = 'ユーザーネームが未入力です。';
+	}
+
+	if ($email == '')
+	{
+		$errors['email'] = 'メールアドレスが未入力です。';
+	}
+
+	//バリデーション突破後
+	if (empty($errors))
+ 	{
+	}
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -19,9 +45,19 @@ if (!empty($_SESSION['id']))
 	<body>
 		<h1>ログイン</h1>
 		<form action="" method="post">
-			<p>ユーザーネーム:<input type="text" name="name"></p>
-			<p>メールアドレス:<input type="text" name="email"></p>
-			<input type="submit" value="ログイン">
+		<p>
+			ユーザーネーム:<input type="text" name="name">
+			<?php if ($errors['name']) : ?>
+				<?php echo h($errors['name']) ?>
+			<?php endif ?>
+		</p>
+		<p>
+			メールアドレス:<input type="text" name="email">
+			<?php if ($errors['email']) : ?>
+				<?php echo h($errors['email']) ?>
+			<?php endif ?>
+		</p>
+		<input type="submit" value="ログイン">
 		</form>
 		<a href="signup.php">新規登録はこちら!</a>
 	</body>
