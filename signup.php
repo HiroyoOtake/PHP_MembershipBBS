aac46d927074aa57523d199734dbb2ea46a6da13
otake <otake.hiroyo@gmail.com>
2017-04-01 12:47:28 +0900

    新規登録画面作成

--- signup.php
+++ signup.php
@@ -1,28 +1,19 @@
 <?php
 
-session_start();
-
-if (!empty($_SESSION['id']))
-{
-		header('Location: index.php');
-		exit;
-}
-
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>ログイン画面</title>
+		<title>新規登録画面</title>
 	</head>
 	<body>
-		<h1>ログイン</h1>
+		<h1>新規登録</h1>
 		<form action="" method="post">
 			<p>ユーザーネーム:<input type="text" name="name"></p>
 			<p>メールアドレス:<input type="text" name="email"></p>
-			<input type="submit" value="ログイン">
+			<input type="submit" value="登録する">
 		</form>
-		<a href="signup.php">新規登録はこちら!</a>
 	</body>
 </html>
