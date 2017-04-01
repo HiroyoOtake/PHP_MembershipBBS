4e02d79242b009aea86d7fc9431cdbc96302ed28
otake <otake.hiroyo@gmail.com>
2017-04-01 12:25:27 +0900

    トップページ作成

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>会員制掲示板</title>
+	</head>
+	<body>
+		<h1>Xさん 会員制掲示板へようこそ!</h1>
+		<p>一言どうぞ!</p>
+		<form action="" method="post">
+			<textarea name="message" cols="30" rows="5"></textarea>
+			<input type="submit" value="投稿する">
+		</form>
+		<hr>
+		<h1>投稿されたメッセージ</h1>
+	</body>
+</html>
